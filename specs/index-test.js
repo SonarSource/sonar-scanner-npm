0ef7e64cdcfd1ff5f206369897b0c6004deb3925
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-12-06 15:42:17 +0100

    apply new prettier formatting

--- specs/index-test.js
+++ specs/index-test.js
@@ -1,8 +1,10 @@
-const assert = require('assert')
-const index = require('../src/index')
+const assert = require('assert');
+const index = require('../src/index');
 
-describe('fromParam', function() {
-  it('should provide the correct identity', function() {
-    assert.deepEqual(index.fromParam(), ['--from=ScannerNpm/' + require('../package.json').version])
-  })
-})
+describe('fromParam', function () {
+  it('should provide the correct identity', function () {
+    assert.deepEqual(index.fromParam(), [
+      '--from=ScannerNpm/' + require('../package.json').version,
+    ]);
+  });
+});
