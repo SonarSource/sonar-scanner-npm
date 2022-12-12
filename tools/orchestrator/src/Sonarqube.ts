f82dc36d5b685d7aae828539eaaa5e792f39a06d
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-12-12 17:46:37 +0100

    refactor with spawn

--- tools/orchestrator/src/Sonarqube.ts
+++ tools/orchestrator/src/Sonarqube.ts
@@ -1,15 +1,9 @@
 import * as path from 'path';
-import { execSync } from 'child_process';
+import { spawn } from 'child_process';
 
 const DEFAULT_FOLDER = path.join(__dirname, '..', 'test', 'cache', 'sonarqube-9.7.1.62043', 'bin', 'macosx-universal-64');
 
 export function start(sqPath: string = DEFAULT_FOLDER) {
   const pathToBin = path.join(sqPath, 'bin', 'macosx-universal-64', 'sonar.sh');
-  try {
-    execSync(`sh ${pathToBin} console`, {stdio: 'inherit'});
-  } catch (error) {
-    console.log('error:', error);
-    console.log('stderr:', (error as any).stdout.toString())
-  }
-
+  return spawn(`${pathToBin}`, ['console'], {stdio: 'inherit'});
 }
--- tools/orchestrator/src/index.ts
+++ tools/orchestrator/src/index.ts
@@ -2,7 +2,16 @@ import { getLatestSonarQube } from './download';
 import { start } from './sonarqube';
 
 (async () => {
-  const latest = await getLatestSonarQube();
-  console.log('finished', latest);
-  start(latest);
+  try {
+    const latest = await getLatestSonarQube();
+    console.log('finished', latest);
+    const process = start(latest);
+    process.on('error', err => {
+      console.log('got', err);
+    })
+    //process.kill('SIGHUP');
+  } catch (error) {
+    console.log('got err', error);
+  }
+
 })();
