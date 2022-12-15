a592ebdc13e05f53809b13663e79bff889024cc7
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-12-15 11:13:09 +0100

    remove SonarCloud test

--- test/integration/scanner.test.js
+++ test/integration/scanner.test.js
@@ -50,37 +50,4 @@ describe('scanner', function () {
       });
     }).timeout(60 * 1000);
   });
-  describe.skip('on SonarCloud', function () {
-    before(async function () {});
-    after(function () {});
-    it('should run', async function () {
-      await scannerPromise({
-        serverUrl: 'https://sonarcloud.io/',
-        token: process.env.SONAR_TOKEN,
-        options: {
-          'sonar.projectName': 'test-scanner',
-          'sonar.organization': 'ilia-kebets-sonarsource',
-          'sonar.sources': path.join(__dirname, '/resources/fake_project_for_integration/src'),
-          //'sonar.tests': './resources/fake_project_for_integration/test'
-        },
-      });
-      const {
-        body: { issues },
-      } = await request.get('https://sonarcloud.io/api/issues/search').query({
-        projects: 'ilia-kebets-sonarsource_sonar-scanner-npm',
-        token: process.env.SONAR_TOKEN,
-      });
-      const myIssues = issues.filter(issue =>
-        issue.component.includes('specs/resources/fake_project_for_integration/src/index.js'),
-      );
-      assert.equal(myIssues.length, 1);
-      const myIssue = myIssues[0];
-      assert.deepEqual(myIssue.textRange, {
-        startLine: 1,
-        endLine: 1,
-        startOffset: 0,
-        endOffset: 7,
-      });
-    }).timeout(60 * 1000);
-  });
 });
