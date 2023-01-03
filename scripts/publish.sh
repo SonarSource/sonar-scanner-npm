d999412cf71bb504677a7394194ceeb964531cf8
David Rautureau <david.rautureau@sonarsource.com>
2023-01-03 15:10:21 +0100

    Fix build (#29)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 PROJECT=sonar-scanner-npm
-VERSION=$(grep version package.json | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g')
+VERSION=$(grep version package.json | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 source cirrus-env BUILD
 npm version --no-git-tag-version --allow-same-version $VERSION-$BUILD_NUMBER
 
@@ -11,8 +11,3 @@ npm version --no-git-tag-version --allow-same-version $VERSION-$BUILD_NUMBER
 jfrog rt npm-publish --build-name=$PROJECT --build-number=$BUILD_NUMBER
 #publish buildinfo
 jfrog rt build-publish $PROJECT $BUILD_NUMBER
-#QA tests could be run now to validate the artifacts and on success we promote.
-#configure jfrog cli to be able to promote build
-jfrog config edit repox --url $ARTIFACTORY_URL --access-token $ARTIFACTORY_PROMOTE_ACCESS_TOKEN
-#promote from QA to public builds
-jfrog rt bpr --status it-passed $PROJECT $BUILD_NUMBER sonarsource-npm-public-builds
