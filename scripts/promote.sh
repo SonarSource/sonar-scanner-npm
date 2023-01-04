b8e08faf5bc8e535ad25e587eae2b4ec594a625b
David Rautureau <david.rautureau@sonarsource.com>
2023-01-04 11:29:42 +0100

    Fix promotion (#30)

--- scripts/promote.sh
+++ scripts/promote.sh
@@ -6,6 +6,6 @@ PROJECT=sonar-scanner-npm
 source cirrus-env PROMOTE
 
 #configure jfrog cli to be able to promote build
-jfrog config edit repox --url $ARTIFACTORY_URL --access-token $ARTIFACTORY_PROMOTE_ACCESS_TOKEN
+jfrog config add repox --url $ARTIFACTORY_URL --access-token $ARTIFACTORY_PROMOTE_ACCESS_TOKEN
 #promote from QA to public builds
 jfrog rt bpr --status it-passed $PROJECT $BUILD_NUMBER sonarsource-npm-public-builds
