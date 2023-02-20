e70fb076c15b7ed9194ecc33b87a64619b656a65
Eric Lorenzana <eric.lorenzana@sonarsource.com>
2023-02-20 18:02:05 +0100

    BUILD-2540 Notify Burgr
    Once this is done, it should show up the releasibility check.

--- scripts/promote.sh
+++ scripts/promote.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+VERSION=$(grep version package.json | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g')
+export VERSION
+
 PROJECT=sonar-scanner-npm
 source cirrus-env PROMOTE
 
@@ -9,3 +12,19 @@ source cirrus-env PROMOTE
 jfrog config add repox --url $ARTIFACTORY_URL --access-token $ARTIFACTORY_PROMOTE_ACCESS_TOKEN
 #promote from QA to public builds
 jfrog rt bpr --status it-passed $PROJECT $BUILD_NUMBER sonarsource-npm-public-builds
+
+# Burgr notification.
+BURGR_FILE=promote.burgr
+cat > $BURGR_FILE <<EOF
+{
+  "version":"${VERSION}",
+  "url":"",
+  "buildNumber":"${BUILD_NUMBER}"
+}
+EOF
+HTTP_CODE=$(curl -s -o /dev/null -w %{http_code} -X POST -d @$BURGR_FILE -H "Content-Type:application/json" -u"${BURGR_USERNAME}:${BURGR_PASSWORD}" "${BURGR_URL}/api/promote/${CIRRUS_REPO_OWNER}/${CIRRUS_REPO_NAME}/${CIRRUS_BUILD_ID}")
+if [ "$HTTP_CODE" != "200" ]; then
+  echo "Cannot notify BURGR ($HTTP_CODE)"
+else
+  echo "BURGR notified"
+fi
