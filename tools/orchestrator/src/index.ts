c6524967d6884a93fc1dedc2a9985b7e31a0eea5
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-11-01 15:47:27 +0100

    update license, and now replace it upon calling `npm run license-fix`

--- tools/orchestrator/src/index.ts
+++ tools/orchestrator/src/index.ts
@@ -1,6 +1,6 @@
 /*
  * sonar-scanner-npm
- * Copyright (C) 2022-2022 SonarSource SA
+ * Copyright (C) 2022-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
@@ -17,6 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 
 export * from './download';
 export * from './sonarqube';
