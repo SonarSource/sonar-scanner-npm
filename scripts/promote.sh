0ea44fc6b24f6257aac48e2ad31859842aea181d
Tibor Blenessy <saberduck@users.noreply.github.com>
2023-11-07 18:21:12 +0100

    fix

--- scripts/promote.sh
+++ scripts/promote.sh
@@ -3,10 +3,12 @@
 set -euo pipefail
 set -x
 
+
+source cirrus-env PROMOTE
+
 PROJECT_VERSION=$(grep version package.json | head -1  | awk -F: '{ print $2 }' | sed 's/[",]//g').$BUILD_NUMBER
 export PROJECT_VERSION
 
-source cirrus-env PROMOTE
 cirrus_jfrog_promote
 
 github-notify-promotion
