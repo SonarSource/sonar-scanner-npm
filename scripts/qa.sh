eff528724a107727e4c6ce47a0e4c939f5f04599
Julien Carsique <julien.carsique@sonarsource.com>
2023-11-17 14:55:20 +0100

    BUILD-4144 fix promotion

--- scripts/qa.sh
+++ scripts/qa.sh
@@ -7,11 +7,11 @@ VERSION=$(grep version package.json | head -1  | awk -F: '{ print $2 }' | sed 's
 source cirrus-env QA
 
 # TODO: https://sonarsource.atlassian.net/browse/BUILD-2397
-# We should have a virtual repo that include npmjs and sonarsource-npm-public-qa
+# We should have a virtual repo that include npmjs and sonarsource-npm-public-qa (ARTIFACTORY_DEPLOY_REPO)
 # and an other one that include npmjs and sonarsource-npm-public-builds
 # The repo npm is wrongly configured and should only include npmjs and sonarsource-npm-public-releases
 npm config set registry https://repox.jfrog.io/artifactory/api/npm/npmjs/
 
 cd test/integration
 npm install
-npm install --no-save "https://$ARTIFACTORY_PRIVATE_USERNAME:$ARTIFACTORY_PRIVATE_PASSWORD@repox.jfrog.io/artifactory/sonarsource-npm-public-qa/sonarqube-scanner/-/sonarqube-scanner-$VERSION-$BUILD_NUMBER.tgz"
+npm install --no-save "https://$ARTIFACTORY_PRIVATE_USERNAME:$ARTIFACTORY_PRIVATE_PASSWORD@repox.jfrog.io/artifactory/$ARTIFACTORY_DEPLOY_REPO/sonarqube-scanner/-/sonarqube-scanner-$VERSION-$BUILD_NUMBER.tgz"
