1579e2bc8d7552b0485475fbedd715593df6577a
Victor <victor.diez@sonarsource.com>
2024-03-12 12:30:09 +0100

    wip

--- scripts/qa.sh
+++ scripts/qa.sh
@@ -10,6 +10,8 @@ source cirrus-env QA
 # We should have a virtual repo that include npmjs and sonarsource-npm-public-qa (ARTIFACTORY_DEPLOY_REPO)
 # and an other one that include npmjs and sonarsource-npm-public-builds
 # The repo npm is wrongly configured and should only include npmjs and sonarsource-npm-public-releases
+npm config set registry https://repox.jfrog.io/artifactory/api/npm/npm/
+
 cd test/integration
 npm install
 npm install --no-save "https://$ARTIFACTORY_PRIVATE_USERNAME:$ARTIFACTORY_PRIVATE_PASSWORD@repox.jfrog.io/artifactory/$ARTIFACTORY_DEPLOY_REPO/sonarqube-scanner/-/sonarqube-scanner-$VERSION-$BUILD_NUMBER.tgz"
