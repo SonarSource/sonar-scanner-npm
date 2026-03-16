d6b5b9384430a7cc0a87560828389d162bc4b3c9
Victor Diez <victor.diez@sonarsource.com>
2026-03-16 11:35:42 +0100

    Fix build: migrate to eslint-plugin-sonarjs

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,27 @@
 const { defineConfig, globalIgnores } = require('eslint/config');
 
 const tsParser = require('@typescript-eslint/parser');
-const notice = require('eslint-plugin-notice');
+const sonarjs = require('eslint-plugin-sonarjs');
+
+const licenseHeader = `/*
+ * sonar-scanner-npm
+ * Copyright (C) 2022-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */`;
 
 module.exports = defineConfig([
   {
@@ -11,18 +31,25 @@ module.exports = defineConfig([
     },
 
     plugins: {
-      notice,
+      sonarjs,
     },
 
     rules: {
-      'notice/notice': [
+      'sonarjs/file-header': [
         'error',
         {
-          templateFile: 'license-header.txt',
-          onNonMatchingHeader: 'replace',
+          headerFormat: licenseHeader,
         },
       ],
     },
   },
-  globalIgnores(['**/dist', '**/build', 'eslint.config.js', '**/fixtures']),
+  globalIgnores([
+    '**/dist',
+    '**/build',
+    '**/coverage/**',
+    '**/.nyc_output/**',
+    'eslint.config.js',
+    '**/fixtures',
+    'bin/**',
+  ]),
 ]);
